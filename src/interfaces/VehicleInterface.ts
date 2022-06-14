d935c3a50f21e5175a11cef32e1f0d989a33f99b
isadora <isadora.bayma@gmail.com>
2022-06-14 13:25:28 -0300

    Refactor: vehicle interface

--- src/interfaces/VehicleInterface.ts
+++ src/interfaces/VehicleInterface.ts
@@ -1,7 +1,13 @@
-export interface Vehicle {
-  model: string,
-  year: number,
-  color: string,
-  status?: boolean,
-  buyValue: number,
-}
+import { z } from 'zod'; 
+
+const vehicleZodSchema = z.object({
+  model: z.string().min(3, { message: 'Model must be at least 3 caracteres' }),
+  year: z.number().gte(1900, { message: 'Year must be at least 1900' })
+    .lte(2022, { message: 'Year cannot be higher than 2021' }),
+  color: z.string().min(3, { message: 'Color must be at least 3 caracteres' }),
+  status: z.boolean().optional(),
+  buyValue: z.number().int(),
+});
+
+type Vehicle = z.infer<typeof vehicleZodSchema>;
+export { Vehicle, vehicleZodSchema };
