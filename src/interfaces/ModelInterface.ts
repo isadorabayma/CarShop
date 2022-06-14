e968a8a30c9d2ee42c6c8a9ead391a54a89ea0bc
isadora <isadora.bayma@gmail.com>
2022-06-14 13:00:55 -0300

    Fix: model generic interface

--- src/interfaces/ModelInterface.ts
+++ src/interfaces/ModelInterface.ts
@@ -1,7 +1,7 @@
-export default interface Model<T> {
+export interface Model<T> {
   create(obj:T): Promise<T>,
   read(): Promise<T[]>,
   readOne(id_:string): Promise<T | null>,
-  update(obj:T): Promise<T | null>,
+  update(id_:string, obj:T): Promise<T | null>,
   delete(id_:string): Promise<T | null>,
 }
