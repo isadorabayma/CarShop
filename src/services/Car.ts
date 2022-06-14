7c3a89166d1acff5fe685389ed6e4116486f8374
isadora <isadora.bayma@gmail.com>
2022-06-14 17:46:37 -0300

    Feat: CarService

---
+++ src/services/Car.ts
@@ -0,0 +1,19 @@
+import Service, { ServiceError } from '.';
+import { Car, carZodSchema } from '../interfaces/CarInterface';
+import CarModel from '../models/Car';
+
+class CarService extends Service<Car> {
+  constructor(model = new CarModel()) {
+    super(model);
+  }
+
+  create = async (obj: Car): Promise<Car | ServiceError | null> => {
+    const parsed = carZodSchema.safeParse(obj);
+    if (!parsed.success) {
+      return { error: parsed.error };
+    }
+    return this.model.create(obj);
+  };
+}
+
+export default CarService;
