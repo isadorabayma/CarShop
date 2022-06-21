0888f210ef7b66d5df1abcd4b5794ea73ef5c868
isadora <isadora.bayma@gmail.com>
2022-06-21 10:11:27 -0300

    Feat: CustomRouter and server config

--- src/server.ts
+++ src/server.ts
@@ -1,18 +1,24 @@
-// import CustomRouter from './routes/Router';
+import CustomRouter from './routes/Router';
 
 import App from './app';
 
 // import exampleController from './controllers/controller-example';
+import CarController from './controllers/Car';
 
 // import { example } from './interfaces/ExampleInterface';
+import { Car } from './interfaces/CarInterface';
 
 const server = new App();
 
 // const exampleController = new exampleController();
+const carController = new CarController();
 
 // const exampleRouter = new CustomRouter<Car>();
+const carRouter = new CustomRouter<Car>();
 // exampleRouter.addRoute(exampleController);
+carRouter.addRoute(carController);
 
 // server.addRouter(exampleRouter.router);
+server.addRouter(carRouter.router);
 
 export default server;
