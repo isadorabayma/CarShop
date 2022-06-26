8f64c2bf083437d6912cb86b3134f52da3592ba2
isadora <isadora.bayma@gmail.com>
2022-06-26 11:44:26 -0300

    test: carModel update and delete modules

--- src/tests/unit/models/car.test.ts
+++ src/tests/unit/models/car.test.ts
@@ -15,12 +15,16 @@ describe('--- Car Model test ---', ()=>{
     sinon.stub(Model, 'create').resolves(createResultMock)
     sinon.stub(Model, 'find').resolves(readResultMock)
     sinon.stub(Model, 'findOne').resolves(createResultMock)
+    sinon.stub(Model, 'findByIdAndUpdate').resolves(createResultMock)
+    sinon.stub(Model, 'findOneAndDelete').resolves(createResultMock)
   })
 
   after(() => {
     (Model.create as sinon.SinonStub).restore();
     (Model.find as sinon.SinonStub).restore();
     (Model.findOne as sinon.SinonStub).restore();
+    (Model.findByIdAndUpdate as sinon.SinonStub).restore();
+    (Model.findOneAndDelete as sinon.SinonStub).restore();
   })
   
   it('1. Create module returns the car created', async ()=>{
@@ -35,10 +39,24 @@ describe('--- Car Model test ---', ()=>{
     expect(result).to.be.equal(readResultMock);  
   })
 
-  it('3. ReadOne module returns a cars in the db by id', async ()=>{
+  it('3. ReadOne module returns a car in the db by id', async ()=>{
 
     const id =  "62b76ebdf8823499b8f9828c";
     const result = await modelInstance.readOne(id);
+    expect(result).to.be.equal(createResultMock);  
+  })  
+  
+  it('4. Update module returns an updated car', async ()=>{
+
+    const id =  "62b76ebdf8823499b8f9828c";
+    const result = await modelInstance.update(id, createBodyMock);
+    expect(result).to.be.equal(createResultMock);  
+  })
+
+  it('5. Delete module returns a cars in the db by id', async ()=>{
+
+    const id =  "62b76ebdf8823499b8f9828c";
+    const result = await modelInstance.delete(id);
     expect(result).to.be.equal(createResultMock);  
   })
 })
